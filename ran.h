47f2cee0735624be665e3ccaab49d60889eeb3cd
Mabach1 <matej.baliga@gmail.com>
2023-06-24 19:25:33 +0200

    adding other functions generating array of random numbers even in range including generating array of uniques

--- ran.h
+++ ran.h
@@ -6,6 +6,9 @@
 #ifdef __MINGW32__  /* these functions work using winddows libraries */
 
 u64 ran_rand(void);
+u64 ran_rand_range(u64 min, u64 max);
+bool ran_rand_arr(u64 *arr, usize size, bool unique);
+bool ran_rand_range_arr(u64 *result, usize size, u64 min, u64 max, bool unique);
 
 #endif
 
