d6920e0dd3fdaf3f3724cd5290a5363866bfaf7b
Mabach1 <matej.baliga@gmail.com>
2023-06-29 21:25:23 +0200

    example of string_print_format() note that the use of function get_lit() completly disrigard the usefulness of print_format() function

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include "base/base.h"
 
 i32 main(i32 argc, char **argv) {
+    if (argc < 2) {
+        ASSERT(0, "Error: Not enough arguments");
+    }
+
     Arena arena;    
 
     arena_init(&arena);
 
     const StringArr args = stringarr_from_args(&arena, argc, argv);
+    
+    string_print_format("message: {} {}\n", args.arr[1], args.arr[2]);
 
-    string_print_format("This file: % is now part of the 'develop' branch", args.arr[0]);
+    fprintf(stdout, "message: %s %s", string_get_lit(args.arr[1]), string_get_lit(args.arr[2]));
 
     arena_deinit(&arena);
 
