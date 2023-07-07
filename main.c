c503ebb018c9d5a7b10eecbbc680956b2bf6dc8a
Mabach1 <matej.baliga@gmail.com>
2023-07-07 19:47:33 +0200

    testing std_print_stream() function, printing to a file

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include "base/base.h"
 
 i32 main(i32 argc, char **argv) {
-    if (argc < 2) {
-        ASSERT(0, "Error: Not enough arguments");
-    }
-
     Arena arena;    
 
     arena_init(&arena);
 
-    const StringArr args = stringarr_from_args(&arena, argc, argv);
-    
-    string_print_format("message: {} {}\n", args.arr[1], args.arr[2]);
+    FILE *file = fopen("text.txt", "w");
 
-    fprintf(stdout, "message: %s %s", string_get_lit(args.arr[1]), string_get_lit(args.arr[2]));
+    ASSERT(file, "Error: coudnt open!\n");
+
+    String message = string_from(&arena, "Ahoj mami");
+
+    std_print_stream(file, "message: {s}", message);
+
+    fclose(file);
 
     arena_deinit(&arena);
 
